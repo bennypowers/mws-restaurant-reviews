86def84257175401439874a2a7ff8b41bc2a7b3e
Benny Powers <benny.p@elasticmedia.io>
2018-02-23 09:17:00 +0200

    chore: attributes guid-generator

--- js/string.js
+++ js/string.js
@@ -1,3 +1,4 @@
+// See https://stackoverflow.com/questions/6860853/generate-random-string-for-div-id
 // S4 :: () -> str
 const S4 = () => (((1+Math.random())*0x10000)|0).toString(16).substring(1);
 
