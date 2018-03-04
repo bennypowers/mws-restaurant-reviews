228656fc83b2ebf2843021890890b646a0fb9802
Benny Powers <web@bennypowers.com>
2018-03-05 01:08:34 +0200

    chore: formats lws.config.js

--- lws.config.js
+++ lws.config.js
@@ -7,17 +7,8 @@ module.exports = {
     'lws-mock-response',
     'lws-static',
     'lws-cors',
-    // 'lws-range',
-    // 'lws-mime',
-    // 'lws-basic-auth',
-    // 'lws-json',
-    // 'lws-rewrite',
-    // 'lws-blacklist',
-    // 'lws-conditional-get',
-    // 'lws-spa',
-    // 'lws-index' ,
   ],
   server: 'http2',
   mocks: 'restaurant-mocks.js',
-  logFormat: 'stats'
+  logFormat: 'stats',
 };
