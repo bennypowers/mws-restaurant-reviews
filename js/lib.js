eca1de72d0b4895cfdc65acab8cb83b58cf90c92
Benny Powers <web@bennypowers.com>
2018-03-14 22:05:06 +0200

    refactor: nice up some lib funcs

--- js/lib.js
+++ js/lib.js
@@ -36,17 +36,26 @@ export const and = (f, g) => function _and() {
  * ARRAY FUNCTIONS
  */
 
+/** Makes array methods point-free. */
+// safeArrayMethod :: m -> f -> as -> b
+export const safeArrayMethod = name => f => as =>
+  (as[name] && typeof as[name] === 'function' ? as : [])[name](f);
+
 /** Point-free array map. Naive implementation. */
 // map :: f -> as -> bs
-export const map = f => m => (m.map ? m : []).map(f);
+export const map = safeArrayMethod('map');
 
-/** Point-free array filter. Naive implementation. */
-// find :: f -> as -> as
-export const filter = f => m => (m.filter ? m : []).filter(f);
+/** Point-free array some. Naive implementation. */
+// some :: f -> as -> as
+export const some = safeArrayMethod('some');
 
 /** Point-free array find. Naive implementation. */
 // find :: f -> as -> a
-export const find = f => m => (m.find ? m : []).find(f);
+export const find = safeArrayMethod('find');
+
+/** Point-free array filter. Naive implementation. */
+// find :: f -> as -> as
+export const filter = safeArrayMethod('filter');
 
 /** Removes duplicates from an array. Naive implementation. */
 // uniq :: as -> as
@@ -57,9 +66,12 @@ export const uniq = compose(Array.from, x => new Set(x));
  * Functions for dealing with Plain Old JavaScript Objects
  */
 
-/** Returns a property by key, or by array of deep keys. */
-// prop :: str -> obj -> a
-export const prop = p => o => p.split('.').reduce((a, b) => a[b], o);
+ /** Returns a property by key, or by dot-separated deep keys. */
+ // prop :: str -> obj -> a
+ export const prop = propertyName => o =>
+   propertyName
+     .split('.')
+     .reduce((a, b) => a[b], o);
 
 /*
  * STRING FUNCTIONS
