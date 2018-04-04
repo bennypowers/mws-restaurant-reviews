b9f360bec69d34c62e8518b5a2059eab26ba029c
Benny Powers <web@bennypowers.com>
2018-04-04 13:57:40 +0300

    fix: fix bug in idb-keyval.js

--- js/idb-keyval.js
+++ js/idb-keyval.js
@@ -18,41 +18,49 @@ async function withStore(type, callback) {
   const db = await getDB();
   return new Promise((resolve, reject) => {
     const transaction = db.transaction('keyval', type);
-    transaction.oncomplete = () => resolve();
-    transaction.onerror = () => reject(transaction.error);
+          transaction.oncomplete = () => resolve();
+          transaction.onerror = () => reject(transaction.error);
     callback(transaction.objectStore('keyval'));
   });
 }
 
-export default class idbKeyval {
+export const get = key => {
+  let req;
+  return withStore('readonly', store => {
+    req = store.get(key);
+  }).then(() => req.result);
+};
 
-  static get(key) {
-    return withStore('readonly', store => store.get(key)).then(req => req.result);
-  }
+const put = (key, value) => store => store.put(value, key);
+export const set = (key, value) =>
+  withStore('readwrite', put(key, value));
 
-  static set(key, value) {
-    return withStore('readwrite', store => store.put(value, key));
-  }
+const _del = key => store => store.delete(key);
+export const del = key =>
+  withStore('readwrite', _del(key));
 
-  static delete(key) {
-    return withStore('readwrite', store => store.delete(key));
-  }
+const _clear = store => store.clear();
+export const clear = () =>
+  withStore('readwrite', _clear);
 
-  static clear() {
-    return withStore('readwrite', store => store.clear());
-  }
+export const keys = async () => {
+  let keys = [];
+  await withStore('readonly', store => {
+    // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
+    // And openKeyCursor isn't supported by Safari.
+    (store.openKeyCursor || store.openCursor).call(store).onsuccess = () => {
+      if (!this.result) return;
+      keys.push(this.result.key);
+      this.result.continue();
+    };
+  });
+  return keys;
+};
 
-  static async keys() {
-    let keys = [];
-    await withStore('readonly', store => {
-      // This would be store.getAllKeys(), but it isn't supported by Edge or Safari.
-      // And openKeyCursor isn't supported by Safari.
-      (store.openKeyCursor || store.openCursor).call(store).onsuccess = () => {
-        if (!this.result) return;
-        keys.push(this.result.key);
-        this.result.continue();
-      };
-    });
-    return keys;
-  }
-}
+export default {
+  clear,
+  del,
+  get,
+  keys,
+  set,
+};
