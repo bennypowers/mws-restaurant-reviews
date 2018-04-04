df3e7a37541d493b14653c4a7350072a1f4ffc31
Benny Powers <web@bennypowers.com>
2018-04-04 14:45:56 +0300

    feat: load restaurants and reviews from cache

--- js/dbhelper.js
+++ js/dbhelper.js
@@ -1,5 +1,16 @@
 import idbKeyval from './idb-keyval.js';
-import { asArray, constant, handleAsJson, handleAsText, rejectNon200, returnOrThrow } from './lib.js';
+import {
+  asArray,
+  compose,
+  constant,
+  filter,
+  handleAsJson,
+  handleAsText,
+  identity,
+  rejectNon200,
+  returnOrThrow,
+  uniq,
+} from './lib.js';
 
 // TODO: All these functions should cache in idb. in case of PUT/POST/DELETE,
 //       they should cache on a unique string (keyed to time?) as pending requests,
@@ -7,30 +18,58 @@ import { asArray, constant, handleAsJson, handleAsText, rejectNon200, returnOrTh
 //       we'll implement background sync.
 
 
+const storeId = key => value => {
+  if (value.id == null) return value;
+  try {
+    const stored = JSON.parse(localStorage[key]);
+    const previous = Array.isArray(stored) ? stored : [];
+    const updated = uniq([...previous, value.id]);
+    localStorage.setItem(key, JSON.stringify(updated));
+  } catch (e) {
+    localStorage.setItem(key, JSON.stringify([value.id]));
+  }
+  return value;
+};
+
 // setKv :: key -> value -> Promise undefined
 const setKv = key => value =>
   idbKeyval.set( value.id ? `${key}/${value.id}` : `${key}`, value );
 
 // cacheInIdb :: String -> d -> Promise value
 const cacheInIdb = key => value =>
-  Promise.all( asArray(value).map( setKv(key) ) )
+  Promise.all( asArray(value).map( compose(setKv(key), storeId(`idb-cached-${key}-ids`)) ) )
     .then( constant(value) );
 
-/** Fetch all restaurants */
-// fetchRestaurants :: () -> Promise rs
-export const fetchRestaurants = () =>
+const getFromIdb = key => compose(
+  idbKeyval.get,
+  x => `${key}/${x}`,
+);
+
+const _fetchRestaurants = () =>
   fetch('/api/restaurants')
     .then(rejectNon200)
     .then(handleAsJson)
     .then(cacheInIdb('restaurants'));
 
+/** Fetch all restaurants */
+// fetchRestaurants :: () -> Promise rs
+export const fetchRestaurants = () =>
+  Promise.all((localStorage['idb-cached-restaurant-ids'] || []).map(getFromIdb('restaurant')))
+    .then(filter(identity))
+    .then(x => x.length ? x : _fetchRestaurants());
+
 /** Fetch reviews by restaurant id. */
 // fetchReviews :: () -> Promise rs
-export const fetchReviews = id => !id ? Promise.resolve(null) :
+const _fetchReviews = id =>
   fetch(`/api/reviews/${id ? `?restaurant_id=${id}` : ''}`)
     .then(rejectNon200)
     .then(handleAsJson)
     .then(cacheInIdb('reviews'));
+
+export const fetchReviews = id => !id ? Promise.resolve(null) :
+  Promise.all((localStorage['idb-cached-review-ids'] || []).map(getFromIdb('reviews')))
+    .then(filter(identity))
+    .then(x => x.length ? x : _fetchReviews(id));
 
 /** Fetch a restaurant by its ID. */
 // fetchRestaurantById :: str -> Promise r
