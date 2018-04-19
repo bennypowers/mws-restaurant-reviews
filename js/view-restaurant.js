c739a617802d6e86229526beec87d30e7b2a2ce0
Benny Powers <web@bennypowers.com>
2018-04-19 17:07:31 +0300

    feat: adds an "Open Now" display to the restaurant page

--- js/view-restaurant.js
+++ js/view-restaurant.js
@@ -1,11 +1,14 @@
 import { putFavorite } from './db/putFavorite.js';
 import { fetchRestaurantById } from './db/fetchRestaurantById.js';
 import { fetchReviews } from './db/fetchReviews.js';
-import { byCreatedAtDesc, placeholderImage, trim } from './lib.js';
+import { $, byCreatedAtDesc, placeholderImage, trim } from './lib.js';
 import { html, render } from '../node_modules/lit-html/lib/lit-extended.js';
+import { until } from '../node_modules/lit-html/lib/until.js';
 import { imageUrlForRestaurant } from './map-marker.js';
 
-const app = document.getElementById('app');
+const intervalSeparator = ', ';
+const hoursSeparator = ' - ';
+
 const upgradeElements = () => {
   Promise.all([
     import('./submit-review.js'),
@@ -14,36 +17,20 @@ const upgradeElements = () => {
   ]);
 };
 
-const reviewCard = ({comments, createdAt, id, updatedAt, name, rating}) =>
-  html`<review-card id="review-${ id }"
-      comments="${ comments }"
-      createdAt="${ createdAt }"
-      updatedAt="${ updatedAt }"
-      name="${ name }"
-      rating="${ rating }"
-  ></review-card>`;
-
-const reviewsList = reviews =>
-    !Array.isArray(reviews) ? html`There was a problem showing the reviews. Please try again.`
-  : !reviews.length ? html`<p>No Reviews Yet!</p>`
-  : reviews
-    .sort(byCreatedAtDesc)
-    .map(reviewCard);
-
 // "optimistic UI"
 const onReviewSubmitted = restaurantId => async () => {
   const reviewsP = fetchReviews(restaurantId);
   const restaurantP = fetchRestaurantById(restaurantId);
   const [restaurant, reviews] = await Promise.all([restaurantP, reviewsP]);
-  return render(restaurantDetails({ restaurant, reviews }), app);
+  return render(restaurantDetails({ restaurant, reviews }), $('#app'));
 };
 
 // takes a string like "11:00 am - 5:00 pm" and returns semantic html
 // str -> str
 const formatOpeningToClosing = string =>
-  string.split(' - ')
+  string.split(hoursSeparator)
     .map(trim)
-    .join(' - ');
+    .join(hoursSeparator);
 
 const days = [
   'Sunday',
@@ -60,18 +47,47 @@ const isToday = dayString => {
   return (dayString.toLowerCase() == today.toLowerCase());
 };
 
-const hoursRowTemplate = ([dayString, hoursString]) => {
-  const today = isToday(dayString);
-  return html`
-  <tr class$="${today ? 'today' : ''}">
-    <td><time>${today ? 'Today' : dayString}</time></td>
-    <td>${hoursString.split(', ').map(formatOpeningToClosing).join(', ')}</td>
-  </tr>`;
+const openTemplate = open => html`
+  <div id="open-now" class$="${ open ? 'open' : 'closed' }">
+    <strong>${ open ? 'Open Now!' : 'Now Closed' }</strong>
+  </div>
+`;
+
+
+const addDays = (...args) =>
+  import('/node_modules/date-fns/esm/addDays/index.js')
+    .then(module => module.default(...args));
+
+const isOpenInInterval = date => async interval => {
+  const { default: isWithinInterval } = await import('/node_modules/date-fns/esm/isWithinInterval/index.js');
+  const { default: parse } = await import('/node_modules/date-fns/esm/parse/index.js');
+  const [opening, closing] = interval.split(hoursSeparator);
+  const start = parse(opening, 'h:mm a', date);
+  const endProvisional = parse(closing, 'h:mm a', date);
+  const endHour = endProvisional.getHours() || 24;
+  const startHour = start.getHours() || 0;
+  const openAfterMidnight = endHour === 24 || startHour > endHour;
+  const end =
+      !openAfterMidnight ? endProvisional
+    : await addDays(endProvisional, 1);
+
+  return isWithinInterval(date, { start, end });
 };
 
-const hoursTemplate = hours =>
-  Object.entries(hours || {})
-    .map(hoursRowTemplate);
+const all = (a, b) => a && b;
+
+const openNow = async (openingHours) => {
+  const date = new Date();
+  const todaysHours = openingHours[ days[ date.getDay() ] ];
+
+  const intervals = await Promise.all(
+    todaysHours
+      .split(intervalSeparator)
+      .map( isOpenInInterval(date) )
+  );
+
+  return intervals.reduce(all, true);
+};
 
 const onCheckedChanged = (restaurant = {}) => event =>
   restaurant.is_favorite != null &&
@@ -86,37 +102,67 @@ const onActiveChanged = event =>
   document.querySelector('submit-review')
     .toggleOpened(event);
 
+const reviewCard = ({comments, createdAt, id, updatedAt, name, rating}) => html`
+  <review-card id="review-${ id }"
+      comments="${ comments }"
+      createdAt="${ createdAt }"
+      updatedAt="${ updatedAt }"
+      name="${ name }"
+      rating="${ rating }"
+  ></review-card>`;
+
+const reviewsList = reviews =>
+    !Array.isArray(reviews) ? html`There was a problem showing the reviews. Please try again.`
+  : !reviews.length ? html`<p>No Reviews Yet!</p>`
+  : reviews
+    .sort(byCreatedAtDesc)
+    .map(reviewCard);
+
+const hoursRowTemplate = ([dayString, hoursString]) => {
+  const today = isToday(dayString);
+  return html`
+  <tr class$="${today ? 'today' : ''}">
+    <td><time>${today ? 'Today' : dayString}</time></td>
+    <td>${hoursString.split(', ').map(formatOpeningToClosing).join(', ')}</td>
+  </tr>`;
+};
+
+const hoursTemplate = hours =>
+  Object.entries(hours || {})
+    .map(hoursRowTemplate);
+
 export const hoursAddressTemplate = ({ address, operating_hours }) => html`
-  <address id="restaurant-address"
-      tabindex="0"
-      aria-label="Address">${address}</address>
+  <section id="restaurant-address-open">
+    <address id="restaurant-address" tabindex="0" aria-label="Address">${address}</address>
+    ${ until( openNow(operating_hours).then(openTemplate), '...' ) }
+  </section>
   <table id="restaurant-hours"
       tabindex="0"
       aria-label="Hours">${hoursTemplate(operating_hours)}</table> `;
 
 export const mapImageTemplate = restaurant => html`
-<figure id="restaurant-image-container">
-  <lazy-image id="restaurant-image"
-      src="${imageUrlForRestaurant(restaurant)}"
-      alt="Interior or exterior of ${name}"
-      placeholder="${placeholderImage}"
-      rootMargin="40px"
-      fade></lazy-image>
-  <figcaption id="restaurant-info">
-    <h2 id="restaurant-name" tabindex="0">${restaurant.name}</h2>
-    <h3 id="restaurant-cuisine">${restaurant.cuisine_type}</h3>
-    <emoji-checkbox id="favourite-checkbox"
-        label="favourite"
-        title="${restaurant.is_favorite ? 'Favourite!' : 'Not Favourite'}"
-        full="😎" empty="💩"
-        checked?="${restaurant.is_favorite}"
-        on-checked-changed="${ onCheckedChanged(restaurant) }"
-    ></emoji-checkbox>
-  </figcaption>
-</figure>`;
+  <figure id="restaurant-image-container">
+    <lazy-image id="restaurant-image"
+        src="${imageUrlForRestaurant(restaurant)}"
+        alt="Interior or exterior of ${name}"
+        placeholder="${placeholderImage}"
+        rootMargin="40px"
+        fade
+    ></lazy-image>
+    <figcaption id="restaurant-info">
+      <h2 id="restaurant-name" tabindex="0">${restaurant.name}</h2>
+      <h3 id="restaurant-cuisine">${restaurant.cuisine_type}</h3>
+      <emoji-checkbox id="favourite-checkbox"
+          label="favourite"
+          title="${restaurant.is_favorite ? 'Favourite!' : 'Not Favourite'}"
+          full="😎" empty="💩"
+          checked?="${restaurant.is_favorite}"
+          on-checked-changed="${ onCheckedChanged(restaurant) }"
+      ></emoji-checkbox>
+    </figcaption>
+  </figure>`;
 
-export const reviewsListTemplate = ({ reviews, restaurant = {} }) => {
-  return html`
+export const reviewsListTemplate = ({ reviews, restaurant = {} }) => html`
   <h2>Reviews</h2>
   <div id="reviews-list">${ reviewsList(reviews) }</div>
 
@@ -128,15 +174,13 @@ export const reviewsListTemplate = ({ reviews, restaurant = {} }) => {
   <submit-review id="review-fab"
       restaurantId="${restaurant.id}"
       on-review-submitted="${ onReviewSubmitted(restaurant.id) }"></submit-review>`;
-};
 
 export const restaurantDetails = ({ reviews = [], restaurant = {} }) => html`
-<div id="map-container">
-  <div id="good-map"></div>
-</div>
-<section id="restaurant-container">${mapImageTemplate(restaurant)}</section>
-<section id="restaurant-details-container">${hoursAddressTemplate(restaurant)}</section>
-<section id="reviews-container" tabindex="0" aria-label="Reviews">${reviewsListTemplate({ reviews, restaurant })}</section>
-`;
+  <div id="map-container">
+    <div id="good-map"></div>
+  </div>
+  <section id="restaurant-container">${mapImageTemplate(restaurant)}</section>
+  <section id="restaurant-details">${hoursAddressTemplate(restaurant)}</section>
+  <section id="reviews-container" tabindex="0" aria-label="Reviews">${reviewsListTemplate({ reviews, restaurant })}</section>`;
 
 requestIdleCallback(upgradeElements);
